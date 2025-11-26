b8ac9659d139c63de09278eda105efcd23779efe
Bhagyakamath <bhagyakamath30@gmail.com>
2025-11-26 12:18:45 +0530

    Thread

--- mavenproject1/src/main/java/com/test/mavenproject1/p6/SchedulerExample.java
+++ mavenproject1/src/main/java/com/test/mavenproject1/p6/SchedulerExample.java
@@ -7,12 +7,19 @@ import java.util.concurrent.TimeUnit;
 public class SchedulerExample {
 
 	public static void main(String[] args) {
+		//Create ScheduledExecutorService with a single thread
 		ScheduledExecutorService scheculer=Executors.newSingleThreadScheduledExecutor();
+		
+		//Define task to be scheduled
 		Runnable task=()->{
 			System.out.println("Task executed at: "+System.currentTimeMillis());
 		};
 		
+		//Schedule the task to run after an initial delay of 1 second, and then repeatedly every 2 seconds
 		scheculer.scheduleAtFixedRate(task, 1, 2, TimeUnit.SECONDS);
+		
+		//Shut down the scheduler after some time, here 10 seconds
+		//It prevents program from running indefinitely
 		scheculer.schedule(()->{
 			scheculer.shutdown();
 			System.out.println("Scheduler shutdown");
