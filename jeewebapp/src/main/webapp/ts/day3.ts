17a9af2286296b6963d3cdcf39b149a4ee966a2b
Bhagyakamath <bhagyakamath30@gmail.com>
2025-12-22 18:00:28 +0530

    Js

--- jeewebapp/src/main/webapp/ts/day3.ts
+++ jeewebapp/src/main/webapp/ts/day3.ts
@@ -7,9 +7,9 @@ function add(x:number, y:number):number{
 }
 
 console.log(h1(10,20,add));
-console.log(h2(10,20,add));
-async function h2(a:number,b:number, callback:Function ):number{
-	var x:number=0;
-	await setTimeout(()=>{x=callback(a,b)},2000);
-	return x;
-}
+//console.log(h2(10,20,add));
+//async function h2(a:number,b:number, callback:Function ):number{
+	//var x:number=0;
+	//await setTimeout(()=>{x=callback(a,b)},2000);
+//	return x;
+//}
